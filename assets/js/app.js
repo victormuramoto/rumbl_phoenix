97a8a3732ea5c85dbb88943ec7ad08d867e5d499
victormuramoto <victor.muramoto@gmail.com>
2021-09-20 18:08:57 +0200

    watch videos with youtube url

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,16 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import "../css/app.scss"
+import Player from "./player"
+
+let video = document.getElementById("video")
+
+console.log(video)
+if (video) {
+  Player.init(video.id, video.getAttribute("data-player-id"), () => {
+    console.log("player ready")
+  })
+}
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -20,6 +20,7 @@ defmodule RumblWeb.Router do
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/videos", VideoController
+    get "/watch/:id", WatchController, :show
     get "/", PageController, :index
   end
 
